9c121a67c613435f240839b1e66d1142eb6e3476
asm-dev <salamanca.m@outlook.com>
2022-10-07 20:07:09 +0200

    Basic data types

---
+++ CodeWars/02-Same case.py
@@ -0,0 +1,18 @@
+def same_case(char1, char2):
+    """
+    This function compares two characters, if either of them isn't a letter it returns -1, when they're both either capitalised or not capitalised
+    (capitalised & capitalised, or not capitalised & not capitalised) it returns 1, and for other cases (taking this as "one is capitalised and the other 
+    isn't, or viceversa) it returns -1
+    """
+    if char1.isalpha() != True or char2.isalpha() != True:
+        return -1
+    elif (char1.islower() and char2.islower()) or (char1.isupper() and char2.isupper()):
+        return 1
+    else:
+        return 0
+
+# print(f"{same_case('a', 'g')}) # == 1")
+# print(f"{same_case('A', 'C')}) # == 1")
+# print(f"{same_case('b', 'G')}) # == 0")
+# print(f"{same_case('B', 'g')}) # == 0")
+# print(f"{same_case('0', '?')}) # == -1")
